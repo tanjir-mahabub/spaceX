8828033d12ef22c303e2e5e0784ea5da01c355d0
Md Tanjir Mahabub <md.tanjir.mahabub@gmail.com>
2022-05-11 17:16:05 +0600

    fetch store created

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,23 @@
+<script>
+    import company from "../stores/fetchStore";
+
+    let url = "https://api.spacexdata.com/v4/company"
+	
+	const [data, loading, error, get] = company(url)
+    
+</script>
+
 <svelte:head>
     <title>Company Info</title>
 </svelte:head>
 <h1 class="text-3xl font-bold text-navy container w-full mx-auto">
     Hello world!
+    
+{#if $loading}
+Loading: {$loading}
+{:else if $error}
+Error: {$error}
+{:else}
+<pre>{JSON.stringify($data, null, 2)}</pre>
+{/if}
 </h1>
