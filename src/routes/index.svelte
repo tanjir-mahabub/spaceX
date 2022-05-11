7638b3d58ffd78d2a151a9fa22fbac878f7c3251
Md Tanjir Mahabub <md.tanjir.mahabub@gmail.com>
2022-05-11 15:21:56 +0600

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,3 @@
+<h1 class="text-3xl font-bold text-navy container w-full mx-auto">
+    Hello world!
+</h1>
---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    colors: {
+      navy: '#001D6E'
+    }
+  },
+  plugins: [],
+}
